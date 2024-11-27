9aca075ecc07cba6295eceb55a8843572c97c3ac
Anja <anja.djakovic@student.etf.unibl.org>
2024-11-27 15:47:19 +0100

    Issue #14: Implement Main gRPC client and gRPC server

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+"""main.py
+
+This Python script implements several functionalities:
+
+    1. Communication with ADC gRPC-client
+
+    2. Communication with Modem gRPC-server
+    
+    3. Communication with Camera via D-Bus
+    
+    4. Reading config file in JSON format in order to get
+        - contact number
+        - servers addresses
+    
+Scripts assignment is to receive data from ADC client and depending on 
+the proximity of the object, to send gRPC-request to Modem gRPC-server
+or to send D-Bus message to Camera
+
+"""
+
 import grpc
 import time
 import logging
@@ -8,35 +28,36 @@ import objectProximityDetectionService_pb2
 import objectProximityDetectionService_pb2_grpc
 from concurrent import futures
 
-channel = None
-stub    = None
+channel = None # connection to modem gRPC server
+stub    = None # connection to modem gRPC server
 
 # Configure logging
 logging.basicConfig(filename='main.log',
                     level=logging.DEBUG,
                     format='%(asctime)s - %(levelname)s - %(message)s')
                     
+# path to the config file in JSON format                    
 config_path = "config.json"
 
 class ObjectProximityDetectionServiceServicer(objectProximityDetectionService_pb2_grpc.ObjectProximityDetectionServiceServicer):
     def ObjectProximityDetection(self, request, context):
         
-        global stub
+        global stub, channel
         
         logging.info(f"Main gRPC-server received gRPC-request from ADC gRPC-client: request.message={request.message}, request.object_proximity_distance={request.object_proximity_distance}")
         print(f"Main gRPC-server received gRPC-request from ADC gRPC-client: request.message={request.message}, request.object_proximity_distance={request.object_proximity_distance}\n")
         
         ########## OBRADA #############
+        THRESHOLD = 0x00000401
         
-        #if request.object_proximity_distance > THRESHOLD1:
-            # D-Bus komunikacija sa drajverom kamere
-        
-        if request.object_proximity_distance > THRESHOLD2:
+        if request.object_proximity_distance > THRESHOLD:
             
             logging.info("Object Detected. Main gRPC-client sends gRPC-request to modem gRPC-server.")
             print("Object Detected. Main gRPC-client sends gRPC-request to modem gRPC-server.\n")
             
-            request_for_modem   = modemCommunication_pb2.ModemCommunicationRequest(message="Object Detected",contact_number=read_contact_number_from_config_file(config_path))
+            number = config_data.read_contact_number_from_config_file(config_path)
+            
+            request_for_modem   = modemCommunication_pb2.ModemCommunicationRequest(message="Object Detected",contact_number=number)
             reply_from_modem    = stub.ModemCommunication(request_for_modem)
             
             logging.info(f"Main gRPC-client received gRPC-reply from modem gRPC-server: {reply_from_modem.message}")
@@ -57,19 +78,22 @@ class ObjectProximityDetectionServiceServicer(objectProximityDetectionService_pb
 def serve_ADC():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     objectProximityDetectionService_pb2_grpc.add_ObjectProximityDetectionServiceServicer_to_server(ObjectProximityDetectionServiceServicer(), server)
-    server.add_insecure_port('127.0.0.1:50051') # Main gRPC server is running on port 50051
+    
+    main_server_address = config_data.read_server_address_from_config_file(config_path, 'main')
+    
+    server.add_insecure_port(main_server_address)
     server.start()
     
-    logging.info("Main gRPC server is running on 127.0.0.1:50051.")
-    print("Main gRPC server is running on 127.0.0.1:50051.\n")
+    logging.info(f"Main gRPC-server is running on {main_server_address}")
+    print(f"Main gRPC-server is running on {main_server_address}\n")
     
     try:
         while True:
-            time.sleep(86400)  # Keep server active
+            time.sleep(86400)  # Keep server active one day
             
     except KeyboardInterrupt:
-        logging.info("Main gRPC server is shuting down.")
-        print("Main gRPC server is shuting down.\n")
+        logging.info("Main gRPC-server is shuting down.")
+        print("Main gRPC-server is shuting down.\n")
         server.stop(0) # Stops the server immediately when a KeyboardInterrupt is raised (e.g., CTRL+C)
 
 """
@@ -81,9 +105,17 @@ def serve_ADC():
 def use_MODEM():
     global stub, channel
     
-    channel = grpc.insecure_channel('127.0.0.1:50052')
+    modem_server_address = config_data.read_server_address_from_config_file(config_path, 'modem')
+    
+    channel = grpc.insecure_channel(modem_server_address)
     stub = modemCommunication_pb2_grpc.ModemCommunicationServiceStub(channel)
 
+
+use_MODEM()
+serve_ADC()
+
+
+"""
 def test():
     
     global stub, channel
@@ -97,8 +129,4 @@ def test():
     logging.info(f"Main gRPC-client received gRPC-reply from modem gRPC-server: {reply_from_modem.message}")
     print(f"Main gRPC-client received gRPC-reply from modem gRPC-server: {reply_from_modem.message}\n")
     
-    
-
-use_MODEM()
-#serve_ADC()
-test()
+"""    
