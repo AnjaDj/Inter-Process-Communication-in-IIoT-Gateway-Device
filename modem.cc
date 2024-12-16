a61b1fbfac8e060dafdd185bb598a8ca46ea9a81
Anja <anja.djakovic@student.etf.unibl.org>
2024-12-16 21:57:18 +0100

    Issue #21: Write Modem C++ aplication as Linux Daemon

--- modem.cc
+++ modem.cc
@@ -9,6 +9,7 @@
 #include "modemCommunication.grpc.pb.h"
 #include <chrono>   
 #include <thread>    
+#include <unistd.h>
 
 
 using grpc::Server;
@@ -21,8 +22,9 @@ using modemCommunication::ModemCommunicationReply;
 using modemCommunication::ModemCommunicationRequest;
 
 std::atomic<bool> server_running(true); // flag
-// Create logger instance
+
 Logger logger("modem.log");
+
 // path to the config file in JSON format  
 std::string config_path = "config_modem.json";                  
 
@@ -31,52 +33,66 @@ class ModemCommunicationServiceImpl final : public ModemCommunicationService::Se
 public:
     Status ModemCommunication(ServerContext* context, const ModemCommunicationRequest* request, ModemCommunicationReply* reply) override
     {
-        logger.log(INFO, "Modem gRPC-server received gRPC-request from Main gRPC-client. Request message: "+request->message());
+        logger.log(INFO, "Modem server received gRPC-request from Main client. Request message: "+request->message());
         
         // Send SMS to contact_number
         int32_t contact_number = request->contact_number();
         logger.log(INFO, "Sending SMS to " + std::to_string(contact_number));
         // Send SMS to contact_number
     
-        std::string reply_for_main = "Modem gRPC-server took action. SMS send.";
-        logger.log(INFO, "Modem gRPC-server sends gRPC-reply to Main gRPC-client. Reply message: "+reply_for_main);
+        std::string reply_for_main = "Modem server took action. SMS sent to " + std::to_string(contact_number);
+        logger.log(INFO, "Modem server sends gRPC-reply to Main client. Reply message: "+reply_for_main);
         reply->set_message(reply_for_main);
 
         return Status::OK;
     }
 };
 
-void serve_main() {
+void serve_main(int max_retries=3) {
     
     std::string   server_address;
     ServerBuilder builder;
     ModemCommunicationServiceImpl service;
     std::unique_ptr<Server> server;
     
-    try{
+    int attempt = 1;
     
-        server_address = read_server_address_from_config_file(config_path, "modem_server_address");
+    while(attempt <= max_retries){
+        try{
         
-        builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-        builder.RegisterService(&service);
-        
-        server = builder.BuildAndStart();
-        
-        if (!server) 
-            throw std::runtime_error("Failed to start the Modem server.");
+            server_address = read_server_address_from_config_file(config_path, "modem_server_address");
+            
+            builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+            builder.RegisterService(&service);
+            
+            server = builder.BuildAndStart();
+            
+            if (!server) 
+                throw std::runtime_error("Modem server failed to start");
+            
+            logger.log(INFO, "Modem server is running on " + server_address);
+            break;
 
-    }catch (std::exception& e) {
-        logger.log(CRITICAL, std::string("Modem gRPC-server failed to start -> ") + e.what());
-        throw; // propagation
+        }catch (std::exception& e) {
+            logger.log(ERROR, "Modem server failed to start -> " + std::string(e.what()) );
+        }
+        
+        attempt++;
+        
+        if (attempt <= max_retries){
+            logger.log(WARNING, "Modem server retrying to start in 3 seconds...");
+            sleep(3);
+        }
+        else{
+            logger.log(CRITICAL, "Modem server reached max startup retries. Unable to start Modem server after multiple attempts!");
+            throw std::runtime_error("Modem server failed to start after multiple attempts.");
+        }
     }
-    
-    logger.log(INFO, "Modem gRPC server is running on " + server_address);
 
-    while (server_running) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    }
-    
-    logger.log(INFO, "Modem gRPC server is shutting down.");
+    while (server_running)
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        
+    logger.log(INFO, "Modem server is shutting down.");
     server->Shutdown();
 }
 
